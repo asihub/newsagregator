e855c0b76ac24b214e36fcdc41c79c4c7e75557b
sazarenko <asi-info@ukr.net>
2019-03-05 13:58:49 +0200

    initial commit

---
+++ src/main/java/com/prf/newsagregator/NewsagregatorApplication.java
@@ -0,0 +1,13 @@
+package com.prf.newsagregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsagregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsagregatorApplication.class, args);
+	}
+
+}
